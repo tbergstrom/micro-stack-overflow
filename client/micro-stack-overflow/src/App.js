d1d5a67bce39d8e1e8b66f0485f16c66eb8a0779
chandler.carne <chandler.carne@outlook.com>
2023-08-10 11:41:22 -0700

    home/nav/login

--- client/micro-stack-overflow/src/App.js
+++ client/micro-stack-overflow/src/App.js
@@ -1,24 +1,85 @@
-import logo from './overflow.svg';
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
+import Nav from './components/Nav';
+import Login from './components/login';
+import Home from './components/Home';
+
+import jwtDecode from 'jwt-decode';
+import AuthContext from './contexts/AuthContext';
+
 
 function App() {
+  
+  const [posts, setPosts] = useState([]);
+  const [user, setUser] = useState(null);
+
+  const login = (token) => {
+    // Decode the token
+    const { sub: username, authorities: authoritiesString } = jwtDecode(token);
+  
+    // Split the authorities string into an array of roles
+    const roles = authoritiesString.split(',');
+  
+    // Create the "user" object
+    const user = {
+      username,
+      roles,
+      token,
+      hasRole(role) {
+        return this.roles.includes(role);
+      }
+    };
+    localStorage.setItem("auth-token", token)
+    setUser(user);
+    return user;
+  };  
+
+  const logout = () => {
+    setUser(null)
+    localStorage.removeItem("auth-token")
+  }
+
+  const auth = {
+    login,
+    logout,
+    user
+  }
+
+  const loadPosts = () => {
+    fetch("http://localhost:8080/api/post")
+    .then(response => response.json())
+    .then(payload => setPosts(payload))
+  }
+
+  useEffect(loadPosts, [])
+
+  useEffect(() => {
+    const token = localStorage.getItem("auth-token")
+    if (token) {
+      login(token)
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Micro Stack Overflow
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ask a Question
-        </a> */}
-      </header>
-    </div>
+    <AuthContext.Provider value={auth}>
+      <BrowserRouter>
+        <Nav />
+
+        <Routes>
+          {/* always visible */}
+          <Route path='/' element={<Home />}/>
+          <Route path="/postlist" element={<PostList />}/>
+          <Route path='*' element={<p>Page Not Found</p>} />
+
+          {/* logged in only */}
+          <Route path='/addpost' element={ user ? <PostForm /> : <Navigate to="/" /> }/>
+
+          {/* logged out only */}
+          <Route path='/login' element={ user ? <Navigate to="/" /> : <Login /> }/>
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
---
+++ client/micro-stack-overflow/src/components/ReplyForm.js
@@ -0,0 +1,117 @@
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams, Link } from "react-router-dom";
+import AuthContext from "../contexts/AuthContext";
+
+const ReplyForm = () => {
+    // const sectionInputState = useState("")
+    // const sectionInputGetter = sectionInputState[0]
+    // const sectionInputSetter = sectionInputState[1]
+
+    const params = useParams()
+    const navigate = useNavigate()
+    const auth = useContext(AuthContext)
+    
+    const [errors, setErrors] = useState([])
+
+    const [body, setBody] = useState("");
+
+
+    const resetState = () => {
+      setBody("")
+    }
+
+    useEffect(() => {
+      if (params.id !== undefined) {
+        
+        fetch(`http://localhost:8080/api/reply/${params.id}`)
+        .then(response => {
+          if (response.ok) {
+            response.json()
+            .then(reply => {
+              setBody(reply.body)
+            })
+          } else {
+            console.log(`Unexpected response status code: ${response.status}`);
+          }
+        })
+      } else {
+        resetState();
+      }
+    }, [params.id])
+
+    const handleSubmit = (evt) => {
+      evt.preventDefault()
+      const newReply = {
+        body: body
+      }
+
+      let url = null;
+      let init = null;
+
+      // TODO can we keep our code dryer?
+      if (params.id !== undefined) {
+        //editing
+        newReply.id = params.id
+        fetch(`http://localhost:8080/api/reply/${params.id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: "Bearer " + auth.user.token
+          },
+          body: JSON.stringify(newReply)
+        })
+        .then(response => {
+          if (response.ok) {
+            navigate("/replylist")
+            resetState()
+          } else {
+            response.json()
+            .then(errors => {
+              setErrors(errors)
+            })
+          }
+        })
+      } else {
+        //adding
+        fetch("http://localhost:8080/api/reply", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: "Bearer " + auth.user.token
+          },
+          body: JSON.stringify(newReply)
+        })
+        .then(response => {
+          if (response.ok) {
+            navigate("/list")
+            resetState()
+          } else {
+            response.json()
+            .then(errors => {
+              setErrors(errors)
+            })
+          }
+        })
+      }
+    }
+    
+    return (
+      <form onSubmit={handleSubmit}>
+        <ul>
+          {errors.map(error => <li key={error}>{error}</li>)}
+        </ul>
+        <fieldset>
+          <label htmlFor="body-input">Title: </label>
+          <input id="body-input" value={section} onChange={(evt) => 
+            setSection(evt.target.value)}/>
+        </fieldset>
+
+        <button type="submit">Save!</button>
+        <Link to="/list">Cancel</Link>
+      </form>
+    )
+}
+
+export default ReplyForm;
