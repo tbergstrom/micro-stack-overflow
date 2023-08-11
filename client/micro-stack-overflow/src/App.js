4c6ab3dd1598b55960cacaa08a995900dd243289
Tyler Bergstrom <tbergstrom05@gmail.com>
2023-08-11 09:13:02 -0700

    fixed post view and reply list

--- client/micro-stack-overflow/src/App.js
+++ client/micro-stack-overflow/src/App.js
@@ -77,7 +77,7 @@ function App() {
           <Route path='/' element={<Home />}/>
           <Route path="/postlist" element={<PostList posts={posts} loadPosts={loadPosts}/>}/>
           <Route path='*' element={<p>Page Not Found</p>} />
-          <Route path="/postview/:id" element={PostView} posts={posts}/>
+          <Route path="/postview/:id" element={<PostView posts={posts} />} />
 
           {/* logged in only */}
           <Route path='/addpost' element={ user ? <PostForm loadPosts={loadPosts}/> : <Navigate to="/" /> }/>
--- client/micro-stack-overflow/src/components/ReplyList.js
+++ client/micro-stack-overflow/src/components/ReplyList.js
@@ -1,26 +1,42 @@
 import { useContext, useEffect, useState } from "react";
 import AuthContext from "../contexts/AuthContext";
+import { useParams } from "react-router-dom";
 
 const ReplyList = (props)=> {
     
     const [replies, setReplies] = useState([]);
     const auth = useContext(AuthContext);
+    const params = useParams();
+
+    const postId = props.postId;
+
+
+    // for post in posts
+    // if post.postId === params.id
+    // then let targetPost = post
+
+    // for reply in replies
+    // if reply.postId === targetPost.postId
+    // display reply
 
     const loadReplies = () => {
-        fetch("http://localhost:8080/api/replylist")
+        fetch(`http://localhost:8080/api/microstackoverflow/reply/${params.id}`) // placeholder URL
         .then(response => response.json())
         .then(payload => setReplies(payload))
     };
+
+    console.log(replies);
     
     useEffect(loadReplies, []);
 
     return (
+        // <p>Replies</p>
         <table>
             <tbody>
                 {replies.map(reply => <tr key={reply.id}>
                     {/* TODO: Figure out how to get the username into this first spot instead of the reply_id */}
-                    <td>{reply.reply_id}</td>
-                    <td>{reply.body}</td>
+                    <td>{reply.replyId}</td>
+                    <td>{reply.replyBody}</td>
                 </tr>)}
             </tbody>
         </table>
